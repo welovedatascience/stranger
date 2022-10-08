66112a978df3c449d2d224b25c1f2902903b6669
ericlecoutre <eric.lecoutre@welovedatascience.com>
2022-10-08 11:03:22 +0000

    add method in Roxygen

--- R/cor.R
+++ R/cor.R
@@ -1,14 +1,14 @@
 
 
 
+#'@export
 setGeneric("cor")
 
 #' Correlation for stranger objects
 #'
-#' describeIn cor Correlation method for stranger objects.
-#' @param x stranger object
-#' @param method see help from base \code{cor} function
-
+#'cor Correlation method for stranger objects.
+#' @method cor stranger
+#' @export
 setMethod("cor",signature(x="stranger"),function(x, method = c("pearson", "kendall", "spearman")){
   selectMethod("cor","ANY")(x[,-'.id',with=FALSE],y=NULL, use="everything",method=method)
 })
