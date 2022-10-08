8e190f2302f7cc93df770c706976893b4d6a919e
ericlecoutre <eric.lecoutre@welovedatascience.com>
2022-10-08 15:30:49 +0000

    change way cor method is implemented for Rxoygen correct documentation

--- R/cor.R
+++ R/cor.R
@@ -1,14 +1,41 @@
-
-
-
-#'@export
-setGeneric("cor")
-
+#' Cor
+#'
 #' Correlation for stranger objects
 #'
-#'cor Correlation method for stranger objects.
-#' @method cor stranger
+#' @param x a stranger  object
+#' @param method see base cor function
+#'
+#' @rdname cor
 #' @export
-setMethod("cor",signature(x="stranger"),function(x, method = c("pearson", "kendall", "spearman")){
-  selectMethod("cor","ANY")(x[,-'.id',with=FALSE],y=NULL, use="everything",method=method)
-})
+cor <- function(x, ...) {
+  UseMethod('cor', x)
+}
+
+#' @rdname cor
+#' @export
+cor.stranger <- function(x, method = c("pearson", "kendall", "spearman"),...) {
+  stats::cor(x[,-'.id',with=FALSE],y=NULL, use="everything",method=method)
+}
+
+#' @rdname cor
+#' @export
+cor.default <- function(x, ...) {
+  return(stats::cor(x, ...))
+}
+
+#' 
+#' 
+#' 
+#' #'@export
+#' setGeneric("cor")
+#' 
+#' #' Correlation for stranger objects
+#' #'
+#' #'cor Correlation method for stranger objects.
+#' #' @method cor stranger
+#' #' @export
+#' setMethod("cor",signature(x="stranger"),function(x, method = c("pearson", "kendall", "spearman")){
+#'   selectMethod("cor","ANY")(x[,-'.id',with=FALSE],y=NULL, use="everything",method=method)
+#' })
+#' 
+#' 
