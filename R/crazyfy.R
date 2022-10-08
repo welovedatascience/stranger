74e93f64de164e413f56c264d753fec40bee43fc
ericlecoutre <eric.lecoutre@welovedatascience.com>
2022-10-08 11:06:37 +0000

    restore crazify latest version

--- R/crazyfy.R
+++ R/crazyfy.R
@@ -40,44 +40,44 @@ crazyfy <- function
   NA.value=0,
   verbose=FALSE)
 #---------------------------------------------------------------------------------
-  {
-
+{
+  
   # stopifnot(require("data.table"))
   # assertthat::assert_that(require("data.table"),msg="data.table required for this function")
-
+  
   ## Checks on parameters
   NA.method=match.arg(NA.method,c("mean","value"))
   do = match.arg(do,c("factor","log","impute","range","scale"),several.ok = TRUE)
   assertthat::assert_that(is.numeric(skewness.cutpoint),msg="Parameter skewness.cutpoint must be a numeric value.")
-
+  
   if (is.vector(data)) data <- as.data.frame(data)
-
+  
   ## Preparation of output
   out <- as.data.table(data)
   meta.preprocess <- list()
   cols <- colnames(out) # get current columns -- ie without ID
   if (!is.null(id)) cols <- cols[!cols%in%id]
   colclasses <- sapply(out[,cols, with=FALSE],class)
-
-
-
+  
+  
+  
   ## Some basic preleminary checks about the variables
-
+  
   ## <CHECK0> Any factor/character but not asked for do: factor <STOP>
   if (any(colclasses %in% c("factor","character")) & !"factor"%in% do) stop("Data contains a factor/character column -- consider removing it or call crazyfy with 'factor' in do parameters.")
-
+  
   ### <CHECK 1> All missings <STOP>
   allmiss <- apply(out,2,function(vec)all(is.na(vec)))
   if (any(allmiss)) stop(paste("Variables",paste(colnames(out)[allmiss],collapse=" and "),"contain only missing values. Please remove them before applying crazyfy."))
-
+  
   ### <CHECK 2> 1 unique value <STOP>
   allunique <- apply(out,2,function(vec)length(unique(vec))==1)
   if (any(allunique)) stop(paste("Variables",paste(colnames(out)[allunique],collapse=" and "),"do not have variance. Please remove them before applying crazyfy."))
-
+  
   ### <CHECK 3> Mode > 50% (all variables) <WARN>
   allmode50 <- apply(out, 2, function(vec) modefreq(vec)$freq > 0.5)
   if (any(allmode50)) warning(paste("Variables",paste(colnames(out)[allmode50],collapse=" and "),"have one mode with frequency higher than 50%. You may want to remove them before applying crazyfy."))
-
+  
   ### <CHECK 4> Equal repartition between categories -- remove ID (!)<STOP>
   do.cols <- cols[colclasses %in% c("factor","character")]
   if (!is.null(id)) do.cols <- do.cols[!do.cols%in%id]
@@ -92,7 +92,7 @@ crazyfy <- function
       stop(paste("Variables",paste(do.cols[factorequal],collapse=" and ")," are factor/character but with equal repartition of categories. Their treatment will thus result in a ITDF with no variance (plus they don't have interest for anomalies).  Please remove them before applying crazyfy."))
     }
   }
-
+  
   ## Create/Assign id
   if (is.null(id))  {
     if (".id" %in% colnames(data)){
@@ -107,14 +107,14 @@ crazyfy <- function
     had.id=TRUE
     setnames(out,id,".id")
   }
-
+  
   ## <CHECK 5> If had.id, check valid one
   if (had.id) assertthat::assert_that(length(unique(out[[".id"]]))==nrow(out), msg="Selected id does not seem to be a valid one.")
-
-
+  
+  
   setkey(out,.id)
-
-
+  
+  
   do_factor <- function(){
     if (verbose) print("crazyfy: FACTOR")
     do.cols <- cols[colclasses %in% c("factor","character")]
@@ -135,13 +135,13 @@ crazyfy <- function
     if (verbose) cat("\nDone.")
     return(out)
   }
-
-
-
+  
+  
+  
   do_log <- function(){
     if (verbose) print("crazyfy: LOG")
     meta.preprocess$log <<- list(done=FALSE,vars=vector(mode="character",length=0),parameters=c(skewness.cutpoint=skewness.cutpoint))
-
+    
     do.cols <- names(out)[sapply(out,is.numeric)]
     do.cols <- do.cols[!do.cols %in% id] # remove ID
     for (ivar in do.cols){
@@ -157,7 +157,7 @@ crazyfy <- function
     if (verbose) cat("\nDone.")
     return(out)
   }
-
+  
   do_impute <- function(){
     if (verbose) print("crazyfy: MISSING")
     meta.preprocess$impute <<- list(
@@ -166,7 +166,7 @@ crazyfy <- function
       parameters=list(NA.method=NA.method, NA.value=NA.value))
     do.cols <- names(out)[sapply(out,is.numeric)]
     do.cols <- do.cols[!do.cols %in% id] # remove ID
-
+    
     if (verbose) print(do.cols)
     for (ivar in do.cols){
       ivar.NA <- is.na(out[[ivar]])
@@ -182,7 +182,7 @@ crazyfy <- function
     if (verbose) cat("\nDone.")
     return(out)
   }
-
+  
   do_range <- function(){
     if (verbose) print("crazyfy: RANGE")
     do.cols <- names(out)[sapply(out,is.numeric)]
@@ -196,7 +196,7 @@ crazyfy <- function
     if (verbose) cat("\nDone.")
     return(out)
   }
-
+  
   do_scale <- function(){
     if (verbose) print("crazyfy: SCALE")
     do.cols <- names(out)[sapply(out,is.numeric)]
@@ -209,17 +209,17 @@ crazyfy <- function
     if (verbose) cat("\nDone.")
     return(out)
   }
-
+  
   ## Apply 'do' functions
   # If 'factor' is one of the method, ensure it is the first one applied
   if("factor" %in% do) do <- unique(c("factor",do))
-
+  
   for (ido in do) out <- get(paste("do", ido, sep = "_"))()
-
+  
   ### Manage duplicated vector of values resulting in 0 distance (problem for some weirds method)
   ### Hypothesis: only data used in computations, ID in .id columns
   ## For data.table adaptation: cf. https://stackoverflow.com/questions/45485373/create-a-mapping-table-of-duplicated-id-keys/45487193#45487193
-
+  
   # print(class(out))
   dup <- as.data.table(out)
   # print(class(dup))
@@ -227,10 +227,10 @@ crazyfy <- function
   dup <- dup[,N:=.N,by=g]
   idmaptable <- dup[N>1,c(".id","g"),with=FALSE]
   setnames(idmaptable,c(".id","g"),c("mappedid",".id"))
-
+  
   out <- dup[!duplicated(dup$g),-c("g","N"),with=FALSE]
-
-
+  
+  
   setattr(out,"meta",list(
     sourcevars=colnames(data)[!(colnames(data)%in% id)],
     id=id,
@@ -239,7 +239,7 @@ crazyfy <- function
     has.duplicates=(nrow(idmaptable)>0),
     duplicated.mapping= idmaptable
   ))
-
+  
   class(out)=c("crazy.data.table",class(out))
   return(out)
 }
